7bcf9c9669189bbe5e517a9ddf0d978107f03017
daniyal <thirstycheems@gmail.com>
2024-01-23 21:18:15 +0500

    final

--- celestial_body.py
+++ celestial_body.py
@@ -1,43 +1,85 @@
 import pygame
+import math
 class Celestial_Body:
     AU = 149.6e6 * 1000
     G = 6.67428e-11
-    scale = 185/AU
+    scale = 200/AU
     time_stamp = 3600*24
-    def __init__(self,x,y,radius,color,mass,sun) -> None:
+    def __init__(self,x,y,radius,color,mass,sun,name) -> None:
         self.x = x
         self.y = y
         self.offset_x = 0
         self.offset_y = 0
-        self.distance_to_sun = x
+        self.distance_to_sun = 0
         self.color = color  
         self.radius = radius
         self.mass = mass
-        self.vx = 0
-        self.vy = 0
         self.sun = sun
         self.distance_to_sun = 0
         self.orbit = []
         self.has_rings = False
+        self.x_vel = 0
+        self.y_vel = 0
+        self.name = name
 
     def move(self,key) -> None:
         if(key == 'u'):
-            self.offset_y -= 5
-        elif(key == 'd'):
             self.offset_y += 5
+        elif(key == 'd'):
+            self.offset_y -= 5
         elif(key == 'r'):
-            self.offset_x += 5
+            self.offset_x -= 5
         elif(key == 'l'):
-            self.offset_x -= 5 
-        print(self.x)
+            self.offset_x += 5
+        
 
-    def draw(self,window,zoom_factor):
+    def attraction(self,other):
+        distance_x = other.x - self.x
+        distance_y = other.y - self.y
+        distance = math.sqrt(distance_x ** 2 + distance_y **2)
+        if other.sun:
+            self.distance_to_sun = distance
+        force = ( self.G * self.mass * other.mass) / distance**2
+        angle = math.atan2(distance_y,distance_x)
+        force_x = math.cos(angle) * force
+        force_y = math.sin(angle) * force
+        return force_x,force_y
+
+    def update_pos(self,planets) -> None:
+        total_fx = total_fy = 0
+        for planet in planets:  
+            if self == planet:  
+                continue
+            fx,fy = self.attraction(planet)
+            total_fx += fx
+            total_fy += fy
+        
+        self.x_vel += total_fx/self.mass * self.time_stamp
+        self.y_vel += total_fy/self.mass * self.time_stamp
+        self.x += self.x_vel * self.time_stamp
+        self.y += self.y_vel  * self.time_stamp
+        self.orbit.append((self.x,self.y))
+    
+    def draw(self,window,zoom_factor,font : pygame.font,sun) -> None:
         height , width = window.get_height(),window.get_width() 
-        x = self.x * self.scale * zoom_factor + width//2 + self.offset_x
-        y = self.y * self.scale * zoom_factor  + height//2 + self.offset_y
+        x = self.x * self.scale * zoom_factor + width//2 + self.offset_x*zoom_factor
+        y = self.y * self.scale * zoom_factor  + height//2 + self.offset_y*zoom_factor
         if(self.has_rings):
             pygame.draw.circle(window,self.color,(x,y),(self.radius + 8)  * zoom_factor)
             pygame.draw.circle(window,(0,0,0,0),(x,y),(self.radius + 5) * zoom_factor)
-        
+
+        if len(self.orbit) > 2:    
+            updated_points = []
+            for point in self.orbit:
+                x,y = point
+                x = x * self.scale * zoom_factor + width/2  + self.offset_x*zoom_factor
+                y = y * self.scale * zoom_factor + height/2 + self.offset_y*zoom_factor
+                updated_points.append((x,y))
+            pygame.draw.lines(window,self.color,False,updated_points)
+        sun_x = sun.x * sun.scale * zoom_factor + width//2 + sun.offset_x*zoom_factor
+        sun_y = sun.y * sun.scale * zoom_factor  + height//2 + sun.offset_y*zoom_factor
+        pygame.draw.line(window, self.color, (sun_x,sun_y), (x,y), 2)  # 2 is the line thickness
         pygame.draw.circle(window,self.color,(x,y),int(self.radius * zoom_factor))    
+        name = font.render(f"{self.name}",1,(0,0,0) if self.sun else (255, 255, 255))
+        window.blit(name,(x - name.get_width(),y - name.get_height()) if not self.sun else (x,y))
 
--- simulation.py
+++ simulation.py
@@ -1,5 +1,4 @@
 import pygame
-import math
 import sys
 from celestial_body import Celestial_Body
 pygame.init()
@@ -9,25 +8,36 @@ pygame.display.set_caption("Planet Simulation")
 class simulation:
     
     def __init__(self) -> None:
-        self.bg = pygame.image.load("./images/bg.jpg")
-        sun = Celestial_Body(0,0,61,(255,255,0),1.98892 * 10**30,True)
-        mercury = Celestial_Body(0.389 * Celestial_Body.AU,0,8,(80,79,81),3.29 * 10**23,False)    
-        venus = Celestial_Body(0.732 * Celestial_Body.AU,0,14,(193,143,23),4.8685 * 10**24,False)    
-        earth = Celestial_Body(-1 * Celestial_Body.AU,0,16,(100,149,237),5.9742 * 10**24,False)    
-        mars = Celestial_Body(-1.524 * Celestial_Body.AU,0,12,(188,39,50),6.39 * 10**23,False)    
-        jupiter = Celestial_Body(-4.2 * Celestial_Body.AU, 0, 29,(227,220,203),1.89 * 10**27,False)
-        saturn = Celestial_Body(5.5 * Celestial_Body.AU, 0, 26,(206,184,184),5.6 * 10**26,False)
-        uranus = Celestial_Body(-19.8 * Celestial_Body.AU, 0, 22,(198,211,227),8.61 * 10**25,False)
-        neptune = Celestial_Body(30 * Celestial_Body.AU, 0, 21,(91,93,223),1.024 * 10**26,False)
-        pluto = Celestial_Body(39 * Celestial_Body.AU, 0, 6,(150,133,112),1.303 * 10**22,False)
-        
+        #bodies
+        sun = Celestial_Body(0,0,60,(255,255,0),1.98892 * 10**30,True,'SUN')
+        mercury = Celestial_Body(0.389 * Celestial_Body.AU,0,8,(80,79,81),3.29 * 10**23,False,'MERCURY')    
+        mercury.y_vel = -47.9 * 1000
+        venus = Celestial_Body(0.732 * Celestial_Body.AU,0,14,(193,143,23),4.8685 * 10**24,False,"VENUS")    
+        venus.y_vel = -35.0 * 1000
+        earth = Celestial_Body(-1 * Celestial_Body.AU,0,16,(100,149,237),5.9742 * 10**24,False,"EARTH")    
+        earth.y_vel = 29.8 * 1000
+        mars = Celestial_Body(-1.524 * Celestial_Body.AU,0,12,(188,39,50),6.39 * 10**23,False,'MARS')    
+        mars.y_vel = 24.1 * 1000
+        jupiter = Celestial_Body(-4.2 * Celestial_Body.AU, 0, 29,(227,220,203),1.89 * 10**27,False,'JUPITER')
+        jupiter.y_vel = 13.1 * 1000
+        saturn = Celestial_Body(9.5 * Celestial_Body.AU, 0, 26,(206,184,184),5.6 * 10**26,False,'SATURN')
+        saturn.y_vel = -9.7 * 1000
+        uranus = Celestial_Body(-19.8 * Celestial_Body.AU, 0, 22,(198,211,227),8.61 * 10**25,False,'URANUS')
+        uranus.y_vel = 6.8 * 1000
+        neptune = Celestial_Body(-30 * Celestial_Body.AU, 0, 21,(91,93,223),1.024 * 10**26,False,'NEPTUNE')
+        neptune.y_vel = 5.4* 1000
+        pluto = Celestial_Body(39 * Celestial_Body.AU, 0, 6,(150,133,112),1.303 * 10**22,False,'BABY PLUTO')
+        pluto.y_vel = 4.64 * 1000
         saturn.has_rings = True
-         
         self.celestial_bodies = [sun,mercury,venus,earth,mars,jupiter,saturn,uranus,neptune,pluto]
+        #zoom
         self.zoom_factor = 1
+        self.font = pygame.font.SysFont('comicsans',12)
+
     def move_screen(self,direction) -> None:
         for planet in self.celestial_bodies:
             planet.move(direction)
+
     def game_loop(self) -> None:    
         clock = pygame.time.Clock();
         while True:
@@ -42,7 +52,6 @@ class simulation:
                     elif (event.key == 45 or event.key == pygame.K_KP_MINUS) and self.zoom_factor > 0.2:
                         self.zoom_factor -= 0.1
                         
-            window.blit(self.bg,(0,0))
             keys = pygame.key.get_pressed()
             if keys[pygame.K_LEFT]:
                 self.move_screen('l')
@@ -52,9 +61,11 @@ class simulation:
                 self.move_screen('u')
             if keys[pygame.K_DOWN]:
                 self.move_screen('d')
+            window.fill((0,0,0))
             for bodies in self.celestial_bodies:
-                bodies.draw(window,self.zoom_factor)
-            pygame.display.flip()
+                bodies.update_pos(self.celestial_bodies)
+                bodies.draw(window,self.zoom_factor,self.font,self.celestial_bodies[0])
+            pygame.display.update()
 
         
 
